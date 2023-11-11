86521d0f325e5c95fc55491fb1f185f367b88892
MaksatAbdimimax <150124339+MaksatAbdimimax@users.noreply.github.com>
2023-11-11 19:04:15 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,18 @@ public class Main {
     public static void main(String[] args) {
                             //0   1     2     3     4     5     6     7     8      9     10    11     12    13    14
         double[] massive = {2.5, 3.5, -4.5, -3.5, 12.5, -7.5, -8.5, 9.5, -23.5, -76.5, -67.5, 54.5, -34.5, 24.5, 9.5};
+        int positive = 0;
+        double negative = 0;
+        boolean nums = false;
         for (double carrentElement:massive ){
-            System.out.println("hi "+ carrentElement);
-
-            System.out.println("gog "+(((massive[4])+(massive[7])+(massive[11])+(massive[13])+(massive[14]))/5));
+           if (carrentElement<0){
+               nums = true;
+           } else if (nums && carrentElement>0) {
+               positive++;
+               negative+=carrentElement;
+           }
         }
-        
+        System.out.println(negative/positive);
 
     }
 }
